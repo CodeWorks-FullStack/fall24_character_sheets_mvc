43b44c49b462824db9bf704ef26067b1ec876cd6
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-26 13:02:12 -0600

    drawing character titles

--- app/controllers/CharactersController.js
+++ app/controllers/CharactersController.js
@@ -1,6 +1,23 @@
+import { AppState } from "../AppState.js";
+
 export class CharactersController {
   // NOTE constructor runs whenever this class is "newed up"
   constructor() {
     console.log('Characters Controller is loaded');
+    this.drawCharacters()
+  }
+
+  // NOTE any method declared within the controller is accessible to the View/DOM/index.html
+  testButton() {
+    console.log('button works');
+  }
+
+  drawCharacters() {
+    const characters = AppState.characters
+    let characterHTML = ''
+    characters.forEach(character => characterHTML += character.title)
+    console.log(characterHTML);
+    const namesElem = document.getElementById('names')
+    namesElem.innerText = characterHTML
   }
 }
--- app/models/Character.js
+++ app/models/Character.js
@@ -4,4 +4,10 @@ export class Character {
     this.className = className
     this.health = 100
   }
+
+  // getters must return a value and can take in no parameters. They accessed as if they are a property of the class object, not a method (no invocation)
+  get title() {
+    const title = `${this.name} the ${this.className}`
+    return title
+  }
 }
