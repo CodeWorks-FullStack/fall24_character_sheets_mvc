26d00540fbe0d05b19c489f7556e455225a486bd
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-26 12:38:37 -0600

    added characters controller to app

--- app/App.js
+++ app/App.js
@@ -1,7 +1,11 @@
+import { CharactersController } from './controllers/CharactersController.js';
+import { HomeController } from './controllers/HomeController.js';
 import { router } from './router-config.js';
 const USE_ROUTER = false
 
 class App {
+  // HomeController = new HomeController()
+  CharactersController = new CharactersController()
 
   constructor() {
     // NOTE ignore this stuff for now
---
+++ app/controllers/CharactersController.js
@@ -0,0 +1,6 @@
+export class CharactersController {
+  // NOTE constructor runs whenever this class is "newed up"
+  constructor() {
+    console.log('Characters Controller is loaded');
+  }
+}
